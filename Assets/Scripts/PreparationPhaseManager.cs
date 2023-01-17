4cb33b299f2c9fb34f85b3f5801f39ce0d9e7d43
codewithJosh <dacasinjoshua21@gmail.com>
2023-01-18 04:49:20 +0800

    feat: add `navigation` button

--- Assets/Scripts/PreparationPhaseManager.cs
+++ Assets/Scripts/PreparationPhaseManager.cs
@@ -1,12 +1,28 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PreparationPhaseManager : MonoBehaviour
 {
-    
+
+    [SerializeField] private Image actionUIButton;
     [SerializeField] private Sprite[] actionNormalUIButtons;
     [SerializeField] private Sprite[] actionPressedUIButtons;
-    [SerializeField] private Image actionUIButton;
+    [SerializeField] private ToggleGroup navigationPanel;
+
+    private enum NavigationStates 
+    { 
+        idle, 
+        results, 
+        upgrades, 
+        staff, 
+        marketing, 
+        recipe, 
+        supplies 
+    };
+
+    private NavigationStates navigationState;
+    private NavigationStates lastNavigationState;
 
     void Start()
     {
@@ -21,6 +37,62 @@ public class PreparationPhaseManager : MonoBehaviour
             SimpleInput.GetButton("OnAction") 
             ? actionPressedUIButtons[1]
             : actionNormalUIButtons[1];
+
+        if (SimpleInput.GetButtonUp("OnNavigation") 
+            || SimpleInput.GetButtonUp("OnAction"))
+            OnNavigation();
+
+    }
+
+    private void OnNavigation()
+    {
+
+        string navigation = GetNavigation(navigationPanel);
+        navigationState = GetNavigationState(navigation);
+
+        if (lastNavigationState != navigationState)
+        {
+
+            FindObjectOfType<GameManager>().GetAnimator.SetInteger("navigationState", (int) navigationState);
+            lastNavigationState = navigationState;
+            /*mangoUINavButton.isOn = true;
+            OnSuppliesQuantityClear();
+            OnSuppliesNavigation(0);
+            capital = FindObjectOfType<Player>().playerCapital;*/
+
+        }
+
+    }
+
+    private string GetNavigation(ToggleGroup _toggleGroup)
+    {
+
+        Toggle navigation = _toggleGroup.ActiveToggles().FirstOrDefault();
+        return navigation.name.ToString();
+
+    }
+
+    private NavigationStates GetNavigationState(string _navigation)
+    {
+
+        return _navigation switch
+        {
+
+            "ResultsUINavButton" => NavigationStates.results,
+
+            "UpgradesUINavButton" => NavigationStates.upgrades,
+
+            "StaffUINavButton" => NavigationStates.staff,
+
+            "MarketingUINavButton" => NavigationStates.marketing,
+
+            "RecipeUINavButton" => NavigationStates.recipe,
+
+            "SuppliesUINavButton" => NavigationStates.supplies,
+
+            _ => NavigationStates.idle,
+
+        };
 
     }
 
