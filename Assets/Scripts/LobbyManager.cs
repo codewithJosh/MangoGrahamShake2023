ebf435389d1de929ead158b3d33c553952b6add4
codewithJosh <dacasinjoshua21@gmail.com>
2023-02-05 00:00:54 +0800

    feat: add `createGame` button

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+
+    [SerializeField] private Image actionUIButton;
+    [SerializeField] private Sprite[] resources;
+
+    private bool isStudent;
+
+    void Start()
+    {
+
+        /*int playerIsStudent = PlayerPrefs.GetInt("player_is_student", -1);
+        isStudent = playerIsStudent == 1;*/
+        isStudent = false;
+    }
+
+    
+    void Update()
+    {
+
+        ActionUIButton = resources[isStudent ? 0 : 1];
+
+        if (SimpleInput.GetButton("OnAction"))
+
+            ActionUIButton = resources[isStudent ? 2 : 3];
+
+    }
+
+    private Sprite ActionUIButton
+    {
+
+        set { actionUIButton.sprite = value; }
+
+    }
+
+}
