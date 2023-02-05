29460cce6a3725e17c69675044f7902372fb8233
codewithJosh <dacasinjoshua21@gmail.com>
2023-02-05 10:58:26 +0800

    feat: add `create` button

--- Assets/Scripts/CreateGameManager.cs
+++ Assets/Scripts/CreateGameManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class CreateGameManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private Button createUIButton;
 
     [SerializeField]
     private TextMeshProUGUI maxPlayersUIText;
@@ -11,11 +15,13 @@ public class CreateGameManager : MonoBehaviour
     [SerializeField]
     private TMP_InputField[] valueUITexts;
 
+    private bool isLoading;
     private int maxPlayers;
 
     void Start()
     {
 
+        isLoading = false;
         maxPlayers = PlayerPrefs.GetInt("max_players", 25);
         MaxPlayers = maxPlayers;
 
@@ -25,37 +31,93 @@ public class CreateGameManager : MonoBehaviour
     void Update()
     {
 
-        bool hasSomeEmpty = RoomName.Equals("")
+        FindObjectOfType<GameManager>()
+            .GetAnimator
+            .SetBool("isLoading", isLoading);
+
+        if (!isLoading)
+        {
+
+            bool hasSomeEmpty = RoomName.Equals("")
             && Password.Equals("")
             && ConfirmPassword.Equals("");
+            bool isEmpty = RoomName.Equals("")
+                || Password.Equals("")
+                || ConfirmPassword.Equals("");
+            bool isConnected = Application.internetReachability != NetworkReachability.NotReachable;
 
-        if (SimpleInput.GetButtonDown("OnCancel"))
+            createUIButton.interactable = isConnected && !isEmpty;
 
-            if (!hasSomeEmpty)
+            if (SimpleInput.GetButtonDown("OnCancel"))
 
-                FindObjectOfType<DialogManager>().OnDialog(
-                    "WARNING",
-                    "Are you sure you're no longer want to create a game?",
-                    "optionPane1"
-                    );
+                if (!hasSomeEmpty)
 
-            else
-                
+                    FindObjectOfType<DialogManager>().OnDialog(
+                        "WARNING",
+                        "Are you sure you're no longer want to create a game?",
+                        "optionPane1"
+                        );
+
+                else
+
+                    SceneManager.LoadScene(2);
+
+            if (SimpleInput.GetButtonDown("OnYes"))
+
                 SceneManager.LoadScene(2);
 
-        if (SimpleInput.GetButtonDown("OnYes"))
-            
-            SceneManager.LoadScene(2);
+            if (SimpleInput.GetButtonDown("OnIncrementMaxPlayers")
+                && MaxPlayers < 50)
 
-        if (SimpleInput.GetButtonDown("OnIncrementMaxPlayers") 
-            && MaxPlayers < 50)
-            
-            MaxPlayers += 1;
+                MaxPlayers += 1;
 
-        if (SimpleInput.GetButtonDown("OnDecrementMaxPlayers")
-            && MaxPlayers > 25 )
+            if (SimpleInput.GetButtonDown("OnDecrementMaxPlayers")
+                && MaxPlayers > 25)
 
-            MaxPlayers -= 1;
+                MaxPlayers -= 1;
+
+            if (SimpleInput.GetButtonDown("OnCreate"))
+            {
+
+                if (!isConnected)
+
+                    FindObjectOfType<DialogManager>().OnDialog(
+                        "NOTICE",
+                        "Please check your internet connection first",
+                        "dialog"
+                        );
+
+                else if (isEmpty)
+
+                    FindObjectOfType<DialogManager>().OnDialog(
+                        "REQUIRED",
+                        "Please fill out all the fields first",
+                        "dialog"
+                        );
+
+                else if (Password.Length < 6)
+
+                    FindObjectOfType<DialogManager>().OnDialog(
+                        "REQUIRED",
+                        "Password must be at least (6) six characters",
+                        "dialog"
+                        );
+
+                else if (!Password.Equals(ConfirmPassword))
+
+                    FindObjectOfType<DialogManager>().OnDialog(
+                        "REQUIRED",
+                        "Password doesn't match",
+                        "dialog"
+                        );
+
+                else
+
+                    Debug.Log("TEST");
+
+            }
+
+        }
 
     }
 
