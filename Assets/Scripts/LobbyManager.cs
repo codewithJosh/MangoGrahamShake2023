e7ae19ffc0f234619a21455084df35b26bd2206a
codewithJosh <dacasinjoshua21@gmail.com>
2023-02-05 16:43:21 +0800

    feat: load `Rooms` into `RoomHUD`

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,3 +1,7 @@
+using Firebase.Extensions;
+using Firebase.Firestore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,6 +12,7 @@ public class LobbyManager : MonoBehaviour
     [SerializeField] private Image actionUIButton;
     [SerializeField] private Sprite[] resources;
 
+    private FirebaseFirestore firebaseFirestore;
     private bool isStudent;
 
     void Start()
@@ -15,9 +20,19 @@ public class LobbyManager : MonoBehaviour
 
         int playerIsStudent = PlayerPrefs.GetInt("player_is_student", -1);
         isStudent = playerIsStudent == 1;
-        
+        Init();
+
     }
 
+    async void Init()
+    {
+
+        await Task.Delay(1);
+        firebaseFirestore = FindObjectOfType<FirebaseFirestoreManager>().Firestore;
+        await Task.Delay(1);
+        LoadRooms();
+
+    }
 
     void Update()
     {
@@ -37,6 +52,45 @@ public class LobbyManager : MonoBehaviour
             else
 
                 SceneManager.LoadScene(3);
+
+    }
+
+    private void LoadRooms()
+    {
+
+        firebaseFirestore
+                .Collection("Rooms")
+                .GetSnapshotAsync()
+                .ContinueWithOnMainThread(task =>
+                {
+
+                    QuerySnapshot documentSnapshots = task.Result;
+
+                    if (documentSnapshots != null && documentSnapshots.Count != 0)
+                    {
+
+                        List<FirebaseRoomModel> rooms = new();
+
+                        foreach (DocumentSnapshot doc in documentSnapshots)
+                        {
+
+                            FirebaseRoomModel room = doc.ConvertTo<FirebaseRoomModel>();
+                            rooms.Add(room);
+
+                        }
+
+                        FindObjectOfType<LoadRoomsManager>().OnLoadRooms(isStudent, rooms);
+
+                    }
+
+                    else
+
+                        FindObjectOfType<DialogManager>().OnDialog(
+                            "EMPTY",
+                            "To get started, let's create a game and invite your students",
+                            "dialog");
+
+                });
 
     }
 
